19371e1286a6f8cd41e2bf34a7e7903955b73383
wawa52520 <877048038@qq.com>
2020-02-20 15:34:45 +0800

    publish.html建立

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -1,11 +1,11 @@
 create table USER
-(
-	ID INTEGER default NEXT VALUE FOR "PUBLIC"."SYSTEM_SEQUENCE_55585DAD_8576_47B8_A4BB_2451FCF55279" auto_increment,
-	ACCOUNT_ID VARCHAR(100),
-	NAME VARCHAR(50),
-	TOKEN CHAR(36),
-	GMT_CREATE BIGINT,
-	GMT_MODIFIED BIGINT,
-	constraint USER_PK
-		primary key (ID)
-);
+    (
+        ID INTEGER default NEXT VALUE FOR "PUBLIC"."SYSTEM_SEQUENCE_55585DAD_8576_47B8_A4BB_2451FCF55279" auto_increment,
+        ACCOUNT_ID VARCHAR(100),
+        NAME VARCHAR(50),
+        TOKEN CHAR(36),
+        GMT_CREATE BIGINT,
+        GMT_MODIFIED BIGINT,
+        constraint USER_PK
+            primary key (ID)
+    );
